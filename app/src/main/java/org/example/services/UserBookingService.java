463e3e93906d5ed6f57f138e5b37c4471aefe84e
lucky <luckymishra26@gmail.com>
2025-05-03 17:44:32 +0530

    used object mapper to map the fields

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -1,11 +1,22 @@
 package org.example.services;
+import java.io.File;
+import java.util.*;
 
 public class UserBookingService {
     
 
     private User user;
+    private List<User> userList;
+
+// object mapper is to serialize and deserialize the json file
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    private static final String USER_PATH = "../localDb/user.json";
 
     public UserBookingService(User user1) {
         this.user = user1;
-    }
+
+        File users =new File(USER_PATH); 
+        userList =new objectMapper.readValue(users, new TypeReference<List<User>>() {});   
 }
